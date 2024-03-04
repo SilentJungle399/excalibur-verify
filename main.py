907cf2a84033267317dc4298906c8c69cac6b88f
SilentJungle399 <vardhan.harsh4041@gmail.com>
2024-03-05 00:52:29 +0530

    added functionality to run scrapers via script

--- main.py
+++ main.py
@@ -1,7 +1,43 @@
+import multiprocessing
+import os
+
 from aiohttp import web
+
+from scrapy import signals
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
 
 app = web.Application()
 router = web.RouteTableDef()
+
+
+def run_crawler(query):
+    process = CrawlerProcess(settings = get_project_settings())
+
+    data = []
+
+    def scrape_finished(item):
+        data.append(item)
+
+    for spider in os.listdir("scraper/search_news_spiders"):
+        if not spider.startswith("__"):
+            process.crawl(spider.strip(".py") + "_search", search_query = query)
+
+    for crawler in process.crawlers:
+        crawler.signals.connect(
+            scrape_finished,
+            signal = signals.item_scraped
+        )
+
+    process.start()
+
+    return data
+
+
+def search_news(query):
+    with multiprocessing.Pool() as pool:
+        data = pool.map(run_crawler, [query])
+        return data
 
 
 @router.post('/image')
@@ -14,7 +50,7 @@ async def index(request):
 @router.post('/text')
 async def index(request):
     text = (await request.json())['text']
-    print(text)
+    news_res = search_news(text)
 
     return web.Response(text = "Hello, world")
 
