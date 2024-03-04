bbf219f910a59d6777d28fb629197ecbc8464156
SilentJungle399 <vardhan.harsh4041@gmail.com>
2024-03-05 00:40:57 +0530

    this works

--- scraper_signal_test.py
+++ scraper_signal_test.py
@@ -1,3 +1,9 @@
+import time
+
+import multiprocessing
+
+from scrapy import signals
+
 from scraper.search_news_spiders.htimes import HindustanTimes
 from scraper.search_news_spiders.livemint import Livemint
 from scraper.search_news_spiders.ndtv import NDTV
@@ -6,11 +12,55 @@ from scraper.search_news_spiders.thehindu import TheHindu
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
-process = CrawlerProcess(settings=get_project_settings())
 
-process.crawl(HindustanTimes)
-process.crawl(Livemint)
-process.crawl(NDTV)
-process.crawl(TheHindu)
+spiders = [
+    HindustanTimes,
+    Livemint,
+    NDTV,
+    TheHindu,
+]
 
-process.start()
+
+def run_crawler(query):
+    process = CrawlerProcess(settings = get_project_settings())
+
+    data = []
+
+    def scrape_finished(item):
+        data.append(item)
+
+    for spider in spiders:
+        process.crawl(spider, search_query = query)
+
+    for crawler in process.crawlers:
+        crawler.signals.connect(
+            scrape_finished,
+            signal = signals.item_scraped
+        )
+
+    process.start()
+
+    return data
+
+
+def launch_crawler(query):
+    t1 = time.time()
+    with multiprocessing.Pool() as pool:
+        data = pool.map(run_crawler, [query])
+        print(len(data), data)
+
+    print(time.time() - t1)
+
+
+launch_crawler("covid")
+print("waiting")
+time.sleep(3)
+
+launch_crawler("covid")
+print("waiting")
+time.sleep(3)
+
+launch_crawler("covid")
+print("waiting")
+time.sleep(3)
+
