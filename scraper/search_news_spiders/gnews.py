7340968d20668451a9f2bc0d6bebbdecb1579630
rishavraj69 <yrishavraj69@gmail.com>
2024-03-07 18:30:35 +0530

    front changed

--- scraper/search_news_spiders/gnews.py
+++ scraper/search_news_spiders/gnews.py
@@ -1,11 +1,16 @@
 import scrapy
 
-class TheHindu(scrapy.Spider):
-    name = "thehindu_latest"
-    start_urls = [
-        "https://www.thehindu.com/latest-news",
-    ]
 
+class GNews(scrapy.Spider):
+    name = "gnews_search"
+    search_query: str
+
+    def start_requests(self):
+        yield scrapy.Request(
+            url = f"https://news.google.com/search?q={self.search_query}",
+            callback = self.parse
+        )
+     
     def parse(self, response: scrapy.http.Response):
         for article in response.css("h3.title a"):
             article_url = article.xpath("@href").get()
